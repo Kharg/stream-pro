68d4e94019f0feb6be8a9436d97e0e33f9de15e1
Andrew Fontana <32223252+Kharg@users.noreply.github.com>
2023-02-20 01:20:47 +0100

    Added Support for Unrelate
    Added support for EspoCRM 7.4.0 Unrelate Note

--- files/client/modules/stream-pro/src/views/stream/notes/unrelate.js
+++ files/client/modules/stream-pro/src/views/stream/notes/unrelate.js
@@ -1,8 +1,7 @@
-define('stream-pro:views/stream/notes/update', 'views/stream/notes/update', function (Dep) {
+define('stream-pro:views/stream/notes/unrelate', 'views/stream/notes/unrelate', function (Dep) {
     return Dep.extend({
         afterRender: function () {
             Dep.prototype.afterRender.call(this);
-            this.$el.find('.details').removeClass('hidden');
             $("div.stream-date-container > * > span").each(function(){
                 var element = $(this).text();
                 var title = $(this).attr('title');
